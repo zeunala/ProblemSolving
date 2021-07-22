58edcb023321a0f6361f64e7f903dc817fd06769
Park je woo <zeunala@naver.com>
2021-07-22 23:41:58 +0900

    Pass/3rd/02:39:57

--- BOJ/2021.07/G5_16234.py
+++ BOJ/2021.07/G5_16234.py
@@ -13,6 +13,9 @@
 * Fail/1st/01:23:38/Timeout
 - N이 50이라 너무 안심하고 최적화를 전혀 하지 않아서 그런 것 같다. 다시해보자.
 * Fail/2nd/01:53:40/Timeout
+- time.time()함수로 분석해보니 countNation함수 부분에서 시간이 너무 많이 쓰여서 함수 호출을 안하고 최대한 시간을 줄이는 방향으로 진행하였다.
+* Pass/3rd/02:39:57
+삼성역량테스트에서 채점하는 데 쓰는 pypy3으로는 정답처리가 되었으나 python3에서는 여전히 시간초과가 발생하였으므로 최적화가 더 필요할 것이다.
 '''
 
 import sys
@@ -50,10 +53,15 @@ for i in range(N):
     A.append(tempInput)
 
 newA = copy.deepcopy(A) # 그냥 newA=A[:]로는 깊은 복사가 안되었다. 아마 이차원배열이라 그런듯?
+
 while True: # 여기서 부터 한단계의 전체과정
+
     link = [[-1 for _ in range(N)] for _ in range(N)] # link배열 -1로 초기화
 
     idx = 0
+    totalIdx = [[] for _ in range(N*N)] # n번 연합에 해당하는 것들
+    sumIdx = [0 for _ in range(N*N)] # n번 연합에 해당하는 것들 총합
+    lenIdx = [0 for _ in range(N*N)] # n번 연합에 해당하는 것들의 개수
     for i in range(N): # 우선 연합을 결정 (BFS 이용)
         for j in range(N):
             if(link[i][j])!=-1: continue # 이미 방문한거면 idx갱신 없이 다음으로
@@ -61,8 +69,13 @@ while True: # 여기서 부터 한단계의 전체과정
             queue = deque([(i, j)])
             while queue:
                 (m, n) = queue.popleft()
-
+                if(link[m][n]==idx): continue
                 link[m][n] = idx
+                totalIdx[idx].append((m, n))
+                sumIdx[idx]+=A[m][n]
+                lenIdx[idx]+=1
+
+
                 if m+1<N:
                     if link[m+1][n]==-1 and abs(A[m][n]-A[m+1][n])>=L and abs(A[m][n]-A[m+1][n])<=R: # 아래쪽 나라와 연합으로 묶일 수 있는지 확인
                         queue.append((m+1, n))
@@ -77,22 +90,16 @@ while True: # 여기서 부터 한단계의 전체과정
                         queue.append((m, n-1))
 
             idx+=1
+    
 
-
-    for i in range(idx): # 그리고 연합별로 이동
-        result = countNation(link, i)
-        resultLen = len(result)
-
-        sum = 0
-        for (m, n) in result: # 각 연합에 대해 합치고
-            sum+=A[m][n]
-        
-        for (m, n) in result: # 분배
-            newA[m][n]=sum//resultLen
+    for i in range(idx): # 그리고 연합별로 합치고 분배
+        for (m, n) in totalIdx[i]: # 분배
+            newA[m][n]=sumIdx[i]//lenIdx[i]
 
     if checkArrSame(A, newA, N): # A를 newA로 갱신하고 변한게 없으면 루프 탈출
         break
 
     answer+=1
+    
 
 print(answer)
