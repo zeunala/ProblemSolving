04f87c99bde4e8fcfa7458178e2571715e79bba7
zeunala <79515820+zeunala@users.noreply.github.com>
2021-12-06 00:34:52 +0900

    Pass/2nd/00:20:00

--- Programmers/2021.10-2021.12/Lv2_17680.py
+++ Programmers/2021.10-2021.12/Lv2_17680.py
@@ -5,6 +5,10 @@
 캐시에 저장할 때 마지막 참조 시간을 저장하고 이 값이 적은것부터 빼면 된다.
 캐시 크기가 작으므로 캐시에서 값을 뺄 때 O(n)이 시간이 걸려도 될 것이다.
 * Fail/1st/00:18:00
+- cache hit가 날 때도 cache를 최신 참조 시각으로 수정하는 것을 빠뜨려서 수정하였다.
+* Pass/2st/00:20:00
+- 다른 사람의 풀이를 보고 생각해보니 번거롭게 딕셔너리 쓸 필요 없이,
+deque으로 큐를 구현해서 코드를 작성해도 충분할 것으로 보인다.
 '''
 def deleteLRU(arr):
     minValue = min(arr.values())
@@ -19,6 +23,7 @@ def solution(cacheSize, cities):
     
     for i in range(len(cities)):
         if cities[i].lower() in cache:
+            cache[cities[i].lower()] = i
             answer += 1
         else:
             if len(cache) < cacheSize:
