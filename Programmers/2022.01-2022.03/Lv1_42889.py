3b32bc1a3d96542ca9012b929305ba74c6df6e81
zeunala <79515820+zeunala@users.noreply.github.com>
2022-01-08 23:49:15 +0900

    Fail/2nd/00:21:48/TimeOver

--- Programmers/2022.01-2022.03/Lv1_42889.py
+++ Programmers/2022.01-2022.03/Lv1_42889.py
@@ -3,6 +3,8 @@
 
 - 주어진 문제 그대로 구현하면 될 것으로 보인다.
 * Fail/1st/00:11:35
+- 스테이지에 도전한 사람이 없을 경우 0으로 나누게 되어 오류가 발생한 것으로 보여 수정하였다.
+* Fail/2nd/00:21:48/TimeOver
 '''
 def getSuccess(targetStage, stages): # targetStage를 클리어한 사람 수 리턴
     return len([stages[i] for i in range(len(stages)) if stages[i] > targetStage])
@@ -17,7 +19,10 @@ def solution(N, stages):
     for i in range(1, N + 1):
         successNum = getSuccess(i, stages)
         failNum = getFail(i, stages)
-        arr.append((-(failNum / (successNum + failNum)), i))
+        if successNum + failNum == 0:
+            arr.append((0, i))
+        else:
+            arr.append((-(failNum / (successNum + failNum)), i))
         
     arr.sort()
     
