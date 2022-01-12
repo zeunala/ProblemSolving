6cfc425fba9e369d6746ab86c914dcb1c1ed10f4
zeunala <79515820+zeunala@users.noreply.github.com>
2022-01-13 00:31:16 +0900

    Pass/3rd/00:32:17

--- Programmers/2022.01-2022.03/Lv2_60057.py
+++ Programmers/2022.01-2022.03/Lv2_60057.py
@@ -5,10 +5,12 @@
 * Fail/1st/00:22:40
 - while문 종료 조건 부분을 수정하였다.
 * Fail/2nd/00:28:49
+- 문자열 크기가 1일 때를 생각하지 않아 틀린 것으로 보이며, answer 초기값을 len(s)로 수정하였다.
+* Pass/3rd/00:32:17
 '''
 
 def solution(s):
-    answer = 9999
+    answer = len(s)
     
     for i in range(1, len(s)//2 + 1): # i는 몇 개 단위로 자를지를 나타낸다.
         result = "" # s를 압축한 후의 문자열
