17a6a35086725b4c987d6a6b119306c86644af85
zeunala <79515820+zeunala@users.noreply.github.com>
2022-02-10 13:20:58 +0900

    Fail/2nd/00:52:59

--- BOJ/2022.01-2022.03/G3_11049.py
+++ BOJ/2022.01-2022.03/G3_11049.py
@@ -14,13 +14,14 @@
 '''
 - DP로 풀어보자. N*N DP배열을 만들어 dp[a][b]는 a번 행렬부터 b번 행렬까지 곱했을 때 필요한 연산 횟수를 저장하도록 한다. (a<b)
 * Fail/1st/00:40:44
+- k의 범위가 잘못된것 같아 수정하였다.
+* Fail/2nd/00:52:59
 '''
 N = int(input())
 matrix = []
 for i in range(N):
     a, b = map(int, input().split())
     matrix.append((a, b))
-    
 dp = [[0 for _ in range(N)] for _ in range(N)]
 for i in range(N):
     for j in range(i+1, N):
@@ -28,10 +29,10 @@ for i in range(N):
     
 for i in range(N-1, -1, -1):
     for j in range(i+1, N):
-        # 기존 dp[i][j]보다 dp[i][k] + dp[k+1][j] + matrix[i][0] * matrix[k+1][0] * matrix[k+1][1] 가 더 작다면 갱신
-        for k in range(j, N-1):
-            temp = dp[i][k] + dp[k+1][j] + matrix[i][0] * matrix[k+1][0] * matrix[k+1][1]
+        # 기존 dp[i][j]보다 중간에 걸친 값이 더 작다면 갱신
+        for k in range(i+1, j):
+            temp = dp[i][k] + dp[k+1][j] + matrix[i][0] * matrix[k][1] * matrix[j][1]
             if dp[i][j] > temp:
                 dp[i][j] = temp
-
+                
 print(dp[0][N-1])
