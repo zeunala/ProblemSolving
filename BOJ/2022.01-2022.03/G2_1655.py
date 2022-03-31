9146de040d4f75fe447c2b64ea6d3354aeaf4f9e
zeunala <zeunala@naver.com>
2022-04-01 01:28:43 +0900

    Pass/4th/01:11:38

--- BOJ/2022.01-2022.03/G2_1655.py
+++ BOJ/2022.01-2022.03/G2_1655.py
@@ -20,6 +20,9 @@ N은 1보다 크거나 같고, 100,000보다 작거나 같은 자연수이다.
 최대힙과 최소힙을 만들어 최대힙에 삽입하되 최대힙과 최소힙의 원소 개수를 같거나 최대힙이 1개 크게 맞춰줘서,
 최대힙에서 가장 큰 원소가 중간값이 되도록 만들면 된다.
 * Fail/3rd/00:59:07
+- 최대힙에 삽입할 때 최대힙의 최대원소보다 새로운 원소가 더 클 경우 최소힙의 최소원소와 교체해주어야지,
+최대힙의 모든 원소가 최소힙의 모든 원소보다 작게 되어 조건을 만족할 수 있게 된다.
+* Pass/4th/01:11:38
 '''
 import sys
 import heapq
@@ -32,11 +35,20 @@ minHeap = []
 for i in range(N):
     num = int(sys.stdin.readline().rstrip())
     
+    # 우선적으로 최대힙에 삽입
     heapq.heappush(maxHeap, -num)
     
+    # 새로 들어온 원소가 최대힙의 최댓값인데 이게 최소힙의 최솟값보다 큰 경우,
+    # 최대힙의 모든 원소 < 최소힙의 모든 원소 조건을 맞추기 위해 원소를 교체한다.
+    if len(minHeap) > 0 and -maxHeap[0] > minHeap[0]:
+        tempA = -heapq.heappop(maxHeap)
+        tempB = heapq.heappop(minHeap)
+        heapq.heappush(maxHeap, -tempB)
+        heapq.heappush(minHeap, tempA)
+    
+    # 최대힙의 원소가 2개이상 많을 경우 크기를 맞춰준다.
     if len(maxHeap) >= len(minHeap) + 2:
-        temp = - heapq.heappop(maxHeap)
+        temp = -heapq.heappop(maxHeap)
         heapq.heappush(minHeap, temp)
     
     print(-maxHeap[0])
-    
