f424f52f8bbb65087c3a91dd5c95630f6b8c9ed4
zeunala <zeunala@naver.com>
2022-04-18 13:48:04 +0900

    Fail/3rd/00:53:54

--- BOJ/2022.04-2022.06/S2_1564.py
+++ BOJ/2022.04-2022.06/S2_1564.py
@@ -14,6 +14,8 @@
 * Fail/1st/00:32:10
 - 맨 앞에 0을 붙여주도록 수정하였다.
 * Fail/2nd/00:41:58
+- 혹시 모를 예외를 대비해 무조건 맨 앞에 0을 붙이지 않도록 하였다.
+* Fail/3rd/00:53:54
 '''
 
 N = int(input())
@@ -26,10 +28,8 @@ for i in range(2, N+1):
     while result % 10 == 0 and result > 0:
         result //= 10
         
-    result %= 100000
+    result %= 1000000
 
-answer = str(result)
-while len(answer) < 5:
-    answer = '0' + answer
-    
+answer = str(result)[-5:]
+
 print(answer)
