854726845f585bbe9b9d8e3536d4c8e94410e84b
zeunala <zeunala@naver.com>
2022-04-20 01:00:57 +0900

    Fail/1st/00:25:49

---
+++ Programmers/2022.04-2022.06/Lv2_77485.py
@@ -0,0 +1,49 @@
+'''
+행렬 테두리 회전하기
+
+- 문제에서 제시한 그대로 구현해보자. 회전 대상 범위를 반복문을 이용하여 하나하나 바꿔나간다.
+* Fail/1st/00:25:49
+'''
+
+def solution(rows, columns, queries):
+    field = [[] for _ in range(rows)]
+    count = 1
+    for i in range(rows):
+        for j in range(columns):
+            field[i].append(count)
+            count += 1
+    
+    answer = []
+    for query in queries:
+        (x1, y1, x2, y2) = query
+        x1 -= 1
+        y1 -= 1
+        x2 -= 1
+        y2 -= 1
+        
+        backup = field[x1 + 1][y1]
+        minNum = backup
+        
+        for j in range(y1, y2 + 1):
+            field[x1][j], backup = backup, field[x1][j]
+            if minNum > backup:
+                minNum = backup
+        
+        for i in range(x1 + 1, x2):
+            field[i][y2], backup = backup, field[i][y2]
+            if minNum > backup:
+                minNum = backup
+        
+        for j in range(y2, y1 - 1, -1):
+            field[x2][j], backup = backup, field[x2][j]
+            if minNum > backup:
+                minNum = backup
+        
+        for i in range(x2 - 1, x1, -1):
+            field[i][x1], backup = backup, field[i][x1]
+            if minNum > backup:
+                minNum = backup
+        
+        answer.append(minNum)
+              
+    return answer
