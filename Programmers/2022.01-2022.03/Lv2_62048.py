9908387981c1eacdb3051308a23223c9300d271c
zeunala <zeunala@naver.com>
2022-05-07 20:21:44 +0900

    Pass/2nd/00:24:00

--- Programmers/2022.01-2022.03/Lv2_62048.py
+++ Programmers/2022.01-2022.03/Lv2_62048.py
@@ -3,15 +3,13 @@
 
 - 세로가 긴 형태로 만들고 각 가로 1칸마다 못쓰게 되는 칸의 수를 구해보자.
 * Fail/1st/00:20:46
+- 선을 그어서 못 쓰는 부분을 합치면 가로 부분 + 세로 부분과 같음을 이용하되 이 때 최대공약수만큼 중복되므로 빼주도록 하자.
+* Pass/2nd/00:24:00
 '''
 import math
 
 def solution(w,h):
-    # h >= w인 상태로 맞추어준다.
-    if w > h:
-        w, h = h, w
-    
     # 사용할 수 없는 블록
-    unable = w * math.ceil(h / w)
+    unable = w + h - math.gcd(w, h)
     
     return w * h - unable
