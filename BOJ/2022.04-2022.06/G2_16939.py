21090ad4ebaab71427b8476e14c07acd46199243
zeunala <zeunala@naver.com>
2022-05-15 01:42:05 +0900

    Fail/2nd/00:30:19

--- BOJ/2022.04-2022.06/G2_16939.py
+++ BOJ/2022.04-2022.06/G2_16939.py
@@ -11,6 +11,8 @@
 '''
 - 총 돌리는 경우의 수는 4가지이므로 4가지 경우만 체크해본다.
 * Fail/1st/00:18:50
+- 돌리는 경우의 수가 2가지 더 있어 총 6가지의 경우를 생각해야한다.
+* Fail/2nd/00:30:19
 '''
 def checkValid(arr):
     # 세로방향으로 위/아래 회전
@@ -21,14 +23,20 @@ def checkValid(arr):
     if arr[1] == arr[3] == arr[21] == arr[23] and arr[5] == arr[7] == arr[2] == arr[4] and arr[9] == arr[11] == arr[2] == arr[4] and arr[22] == arr[24] == arr[6] == arr[8] and arr[13] == arr[14] == arr[15] == arr[16] and arr[17] == arr[18] == arr[19] == arr[20]:
         return 1
     
-    
     # 가로방향으로 왼쪽/오른쪽 회전
     if arr[5] == arr[6] == arr[19] == arr[20] and arr[17] == arr[18] == arr[23] == arr[24] and arr[21] == arr[22] == arr[15] == arr[16] and arr[13] == arr[14] == arr[7] == arr[8] and arr[1] == arr[2] == arr[3] == arr[4] and arr[9] == arr[10] == arr[11] == arr[12]:
         return 1
     
-    
     # 가로방향으로 오른쪽/왼쪽 회전
     if arr[5] == arr[6] == arr[15] == arr[16] and arr[17] == arr[18] == arr[7] == arr[8] and arr[21] == arr[22] == arr[19] == arr[20] and arr[13] == arr[14] == arr[23] == arr[24] and arr[1] == arr[2] == arr[3] == arr[4] and arr[9] == arr[10] == arr[11] == arr[12]:
+        return 1
+    
+    # 정면방향으로 시계방향 회전
+    if arr[1] == arr[2] == arr[17] == arr[19] and arr[18] == arr[20] == arr[9] == arr[10] and arr[11] == arr[12] == arr[14] == arr[16] and arr[13] == arr[15] == arr[3] == arr[4] and arr[5] == arr[6] == arr[7] == arr[8] and arr[21] == arr[22] == arr[23] == arr[24]:
+        return 1
+    
+    # 정면방향으로 반시계방향 회전
+    if arr[1] == arr[2] == arr[14] == arr[16] and arr[18] == arr[20] == arr[3] == arr[4] and arr[11] == arr[12] == arr[17] == arr[19] and arr[13] == arr[15] == arr[9] == arr[10] and arr[5] == arr[6] == arr[7] == arr[8] and arr[21] == arr[22] == arr[23] == arr[24]:
         return 1
     
     return 0
