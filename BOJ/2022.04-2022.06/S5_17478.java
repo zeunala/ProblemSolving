86f0733e08cb311f5c18d8d9d4d1be2c301c30a0
zeunala <zeunala@naver.com>
2022-06-20 00:54:24 +0900

    Pass/3rd/00:13:03

--- BOJ/2022.04-2022.06/S5_17478.java
+++ BOJ/2022.04-2022.06/S5_17478.java
@@ -11,25 +11,30 @@
 * Fail/1st/00:09:25
 - 맨 첫줄은 반복을 하지 않도록 수정하였다.
 * Fail/2nd/00:10:49
+- 문제의 조건을 제대로 읽고 수정하였다.
+* Pass/3rd/00:13:03
 */
 import java.util.Scanner;
 
 public class S5_17478 {
    static void printChat(int current, int end) { // 현재 current번째, 총 end번을 해야한다.
-        if (current > end)
-            return;
-
         String indent = "";
         for (int i = 1; i <= current; i++) {
             indent += "____";
         }
 
-        System.out.println(indent + "\"재귀함수가 뭔가요?\"");
-        System.out.println(indent + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-        System.out.println(indent + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-        System.out.println(indent + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-        printChat(current + 1, end);
-        System.out.println(indent + "라고 답변하였지.");
+        if (current == end) {
+            System.out.println(indent + "\"재귀함수가 뭔가요?\"");
+            System.out.println(indent + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            System.out.println(indent + "라고 답변하였지.");
+        } else {
+            System.out.println(indent + "\"재귀함수가 뭔가요?\"");
+            System.out.println(indent + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+            System.out.println(indent + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+            System.out.println(indent + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+            printChat(current + 1, end);
+            System.out.println(indent + "라고 답변하였지.");
+        }
     }
 
     public static void main(String[] args) {
