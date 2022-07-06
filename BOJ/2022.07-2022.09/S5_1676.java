321ed94eedb34e03ab1110e53a92fd12ec466859
zeunala <zeunala@naver.com>
2022-07-07 07:26:25 +0900

    Pass/3rd/00:16:44

--- BOJ/2022.07-2022.09/S5_1676.java
+++ BOJ/2022.07-2022.09/S5_1676.java
@@ -12,6 +12,10 @@
 - 수를 한자리만 남기고 지우면 예를 들어 25*4 = 100이 되어야 할 것이 5*4 = 20이 되어 0의 개수가 줄어들 수 있다.
 더 많은 자리수를 남기도록 수정하였다.
 * Fail/2nd/00:14:58
+- 더 많은 자리수를 남기도록 수정하는 과정에서 곱한 결과가 int범위를 넘어서는 문제가 발생할 수 있다.
+long 자료형을 사용하도록 수정하였다.
+* Pass/3rd/00:16:44
+- 문제를 푼 후 다른 풀이를 본 결과, 10 = 2 * 5에서 착안하여 2와 5가 곱해진 개수를 체크하는 방법도 있음을 알 수 있었다.
 */
 import java.util.*;
 
@@ -20,7 +24,7 @@ public class S5_1676 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int answer = 0;
-        int temp = 1;
+        long temp = 1;
 
         for (int i = 1; i <= N; i++) {
             temp *= i;
