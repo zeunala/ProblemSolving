175787f1282d7d24c2c77e861ce6e3900f5b91d1
zeunala <zeunala@naver.com>
2022-07-22 00:06:27 +0900

    Fail/1st/00:02:35

---
+++ BOJ/2022.07-2022.09/S5_9655.java
@@ -0,0 +1,28 @@
+/*
+돌 게임
+
+입력
+첫째 줄에 N이 주어진다. (1 ≤ N ≤ 1000)
+
+출력
+상근이가 게임을 이기면 SK를, 창영이가 게임을 이기면 CY을 출력한다.
+
+- N이 4의 배수이면 후공이, 그렇지 않으면 선공이 이기게 된다.
+* Fail/1st/00:02:35
+*/
+import java.util.*;
+
+public class S5_9655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        if (N % 4 == 0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+
+        sc.close();
+    }
+}
