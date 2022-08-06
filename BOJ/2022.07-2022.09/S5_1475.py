891529cc71f239dfb04956f9228828838b92c490
zeunala <zeunala@naver.com>
2022-08-06 23:42:53 +0900

    Fail/1st/00:05:54

---
+++ BOJ/2022.07-2022.09/S5_1475.py
@@ -0,0 +1,27 @@
+'''
+방 번호
+
+입력
+첫째 줄에 다솜이의 방 번호 N이 주어진다. N은 1,000,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 필요한 세트의 개수를 출력한다.
+'''
+
+'''
+- 딕셔너리를 이용하여 0~9의 각 등장 횟수를 기록해서 최댓값을 출력하도록 한다. 이 때 6과 9에 주의한다.
+* Fail/1st/00:05:54
+'''
+from collections import defaultdict
+
+inputString = input()
+numDict = defaultdict(int)
+
+for e in inputString:
+    numDict[e] += 1
+    
+while numDict['6'] < numDict['9']: # 6과 9의 균형을 맞춘다.
+    numDict['6'] += 1
+    numDict['9'] -= 1
+
+print(max(numDict.values()))
