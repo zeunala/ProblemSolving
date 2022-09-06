eb397ce0685a6328f65135511928fa40b0b5ecbe
zeunala <zeunala@naver.com>
2022-09-06 11:28:35 +0900

    Pass/2nd/00:10:20

--- BOJ/2022.07-2022.09/G5_1174.py
+++ BOJ/2022.07-2022.09/G5_1174.py
@@ -11,6 +11,8 @@ N이 주어진다. N은 1,000,000보다 작거나 같은 자연수이다.
 '''
 - 9876543210의 부분문자열(연속일 필요성X)들을 크기 순으로 나열하도록 한다.
 * Fail/1st/00:08:48
+- 문자열을 뽑을 때 최대 10개까지 뽑을 수 있도록 수정하였다.
+* Pass/2nd/00:10:20
 '''
 from itertools import combinations
 
@@ -19,7 +21,7 @@ N = int(input())
 num = "9876543210"
 arr = [-1]
 
-for i in range(1, 10):
+for i in range(1, 11):
     tempArr = []
     
     for e in list(combinations("9876543210", i)): # 예를 들어 i = 2라면 e는 ('9', '7') 같은 식이 됨
@@ -30,7 +32,7 @@ for i in range(1, 10):
     
     tempArr.sort()
     arr.extend(tempArr)
-    
+
 if N >= len(arr):
     print(-1)
 else:
