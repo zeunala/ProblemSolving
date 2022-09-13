6d538a84cf418b6db41ca24847568baf8f2b80bd
zeunala <zeunala@naver.com>
2022-09-13 12:17:20 +0900

    Pass/3rd/00:50:12

--- BOJ/2022.07-2022.09/G5_2293.py
+++ BOJ/2022.07-2022.09/G5_2293.py
@@ -15,6 +15,8 @@
 * Fail/1st/00:26:26
 - 메모리초과를 막도록 dp를 두 줄만 사용하도록 수정하였다.
 * Fail/2nd/00:31:28
+- dp갱신하는 부분을 일부 수정하였다.
+* Pass/3rd/00:50:12
 '''
 n, k = map(int, input().split())
 coinPrice = []
@@ -23,7 +25,7 @@ for i in range(n):
     coinPrice.append(int(input()))
 coinPrice.sort()
 
-dp = [0] * (k + 1) # dp[-1][a]는 동전을 이용해서 a원을 만드는 경우의 수
+dp = [0] * (k + 1) # dp[a]는 동전을 이용해서 a원을 만드는 경우의 수
 dpPrev = [] # 이전 dp 저장
 
 # 첫번째 동전만을 사용하는 경우를 채움
@@ -43,6 +45,6 @@ for i in range(1, n):
         if j - coinPrice[i] < 0:
             dp[j] = max(dp[j], dpPrev[j])
         else:
-            dp[j] = dp[j - coinPrice[i]] + max(dpPrev[j - coinPrice[i]], dpPrev[j])
+            dp[j] = dp[j - coinPrice[i]] + dpPrev[j]
         
 print(dp[-1])
