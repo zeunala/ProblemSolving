570e971df9d3d95fdd3b0c48c005ef38f77bdfdb
zeunala <zeunala@naver.com>
2022-11-17 02:31:44 +0900

    Pass/2nd/00:15:13

--- BOJ/2022.10-2022.12/P2_11694.py
+++ BOJ/2022.10-2022.12/P2_11694.py
@@ -14,17 +14,26 @@ koosaga가 이기는 경우에는 'koosaga'를, cubelover가 이기는 경우에
 이미 XOR연산의 결과가 0이라면 후공이 승리, 그렇지 않다면 선공이 승리한다.
 다만, 예외적으로 1 하나만 있으면 후공이, 1 두 개가 있으면 선공이 승리한다.
 * Fail/1st/00:08:45
+- 1, 1 1 뿐만 아니라 1만 있는 상황 모두가 예외상황에 속한다.
+따라서 모든 숫자가 1일 때, 1의 개수가 홀수개라면 후공이, 1의 개수가 짝수개라면 선공이 승리한다.
+* Pass/2nd/00:15:13
+- 님 게임을 이전에 알고 있었기에 어렵지 않게 풀 수 있었지만,
+문제 풀이 이후 다른 사람의 풀이를 본 결과 이런류의 게임을 일반화하여 스프라그-그런디 정리를 이용할 수 있음을 알게 되었다.
 '''
 N = int(input())
 arr = list(map(int, input().split()))
 
 resultXOR = 0
+isAllOne = True
 for i in range(len(arr)):
     resultXOR ^= arr[i]
+    if arr[i] != 1:
+        isAllOne = False
+
+if isAllOne: # 모든 숫자가 1인 상황에서는 resultXOR이 1 아니면 0 이 되므로 ^=1을 해주면 승리자가 바뀌게 된다.
+    resultXOR ^= 1
     
-if len(arr) == 1 and arr[0] == 1:
-    print("cubelover")
-elif (len(arr) == 2 and arr[0] == 1 and arr[1] == 1) or (resultXOR != 0):
+if resultXOR != 0:
     print("koosaga")
 else:
     print("cubelover")
