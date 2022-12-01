2512644f2e698556323921c269569f85f41509d2
zeunala <zeunala@naver.com>
2022-12-01 21:45:11 +0900

    Fail/3rd/00:39:20

--- BOJ/2022.04-2022.06/S3_2579.java
+++ BOJ/2022.04-2022.06/S3_2579.java
@@ -13,6 +13,10 @@
 * Fail/1st/00:17:25
 - 끝 2칸 전에서의 조건을 수정하였다.
 * Fail/2nd/00:20:18
+- 5 100 100 100 100 1 의 경우 1, 2, 4, 5번째 칸을 밟아 301까지 가능하지만 201이 나오는 반례가 있다.
+시작점은 계단에 포함되지 않기에 arr[0] -> arr[1] -> arr[2]가 가능한데 이를 놓친 것으로 보인다.
+또한 else if (i == N - 3) 부분은 불필요하므로 삭제해도 될 것이다.
+* Fail/3rd/00:39:20
 */
 import java.lang.*;
 import java.util.*;
@@ -34,11 +38,17 @@ public class S3_2579 {
             } else if (i == N - 1) { // 끝 1칸 전인 경우 현재 칸과 다음 칸의 점수 먹고 종료
                 dp[i] = arr[i] + arr[i + 1];
             } else if (i == N - 2) { // 끝 2칸 전의 경우 현재 칸 + 다다음칸 점수 먹고 종료
-                dp[i] = arr[i] + arr[i + 2];
-            } else if (i == N - 3) { // 끝 3칸 전의 경우 1칸 + 2칸 가는 경우와 2칸 + 1칸 가는 경우 중 최댓값
-                dp[i] = arr[i] + Math.max(arr[i + 1] + arr[i + 3], arr[i + 2] + arr[i + 3]);
+                if (i == 0) { // 시작점인 경우 연달아 세 칸을 건널 수도 있다.
+                    dp[i] = arr[i] + arr[i + 1] + arr[i + 2];
+                } else {
+                    dp[i] = arr[i] + arr[i + 2];
+                }
             } else { // 그 외에는 1칸/2칸, 2칸 중 최댓값 선택
-                dp[i] = arr[i] + Math.max(arr[i + 1] + dp[i + 3], dp[i + 2]);
+                if (i == 0) { // 시작점인 경우 1칸/1칸, 1칸/2칸이 가능하다. (계단점수가 자연수이므로 2칸보다 1칸/1칸이 무조건 유리)
+                    dp[i] = arr[i] + Math.max(arr[i + 1] + dp[i + 3], arr[i + 1] + dp[i + 2]);
+                } else {
+                    dp[i] = arr[i] + Math.max(arr[i + 1] + dp[i + 3], dp[i + 2]);
+                }
             }
         }
         System.out.println(dp[0]);
