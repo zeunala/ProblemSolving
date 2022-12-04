38f51f41d43efdad9f583a580d016793478447bc
zeunala <zeunala@naver.com>
2022-12-04 12:39:31 +0900

    Pass/2nd/00:14:08

--- BOJ/2022.10-2022.12/S5_2057.py
+++ BOJ/2022.10-2022.12/S5_2057.py
@@ -11,6 +11,9 @@
 '''
 - 입력 제한이 20!보다 작다. 따라서 0!~19! 중 모든 경우의 수를 생각해본다. (2 ** 20은 100만정도밖에 되지 않음을 이용)
 * Fail/1st/00:09:13/MemoryLimitExceeded
+- product 대신 dfs를 이용하도록 수정하였다.
+* Pass/2nd/00:14:08
+- 문제 풀이 이후 다른 사람의 풀이를 본 결과 그냥 19!부터 0!까지 뺄 수 있는만큼 빼주는 방식으로 구현할 수 있음을 알게 되었다.
 '''
 from itertools import product
 
@@ -20,16 +23,21 @@ def fact(num):
         result *= i
     return result
 
-N = int(input())
 factArr = [fact(i) for i in range(20)] # factArr[i]는 i!의 값 저장
-allCase = list(product((0, 1), repeat = len(factArr)))
-
 factSum = set() # 19!까지의 가능한 모든 팩토리얼 합들을 저장
-for case in allCase:
-    tempSum = 0
-    for i in range(len(case)):
-        tempSum += factArr[i] * case[i]
-    factSum.add(tempSum)
+def dfs(case):
+    if len(case) == 20:
+        tempSum = 0
+        for i in range(len(case)):
+            tempSum += factArr[i] * case[i]
+        factSum.add(tempSum)
+        return
+    
+    dfs(case + [0])
+    dfs(case + [1])
+        
+N = int(input())
+dfs([])
     
 if N != 0 and N in factSum: # 0은 예외로 제거
     print("YES")
