8c8b7a58f0048f1d930005c5e0186b3cef4e3d70
zeunala <zeunala@naver.com>
2023-02-25 18:02:47 +0900

    Pass/2nd/00:19:19

--- BOJ/2023.01-2023.03/S3_16508.py
+++ BOJ/2023.01-2023.03/S3_16508.py
@@ -15,34 +15,40 @@ Wi는 항상 대문자이다.
 '''
 - N의 범위가 작으므로 모든 경우의 수를 탐색하도록 한다.
 * Fail/1st/00:11:36
+- 알파벳 여부만 확인하지 않고 개수가 맞는지까지 확인하도록 수정하였다.
+* Pass/2nd/00:19:19
 '''
 from itertools import product
+from collections import defaultdict
 
 def isValid(alphaArr, target): # alphaArr로 target 문자를 만들 수 있는지 여부 리턴
+    alphaDict = defaultdict(int) # 각 알파벳 별로 몇 번 등장하는지
+    for alpha in alphaArr:
+        for e in alpha:
+            alphaDict[e] += 1
+    
     for e in target:
-        isExist = False
-        for alpha in alphaArr:
-            if e in alpha:
-                isExist = True
-        if isExist == False:
+        if alphaDict[e] > 0:
+            alphaDict[e] -= 1
+        else:
             return False
     return True
     
 T = input()
 N = int(input())
-alphaArr = [set() for _ in range(N)] # alphaArr[i]는 i번째(i>=0) 전공책에 들어있는 알파벳들
+alphaArr = [list() for _ in range(N)] # alphaArr[i]는 i번째(i>=0) 전공책에 들어있는 알파벳들
 priceArr = [0 for _ in range(N)] # priceArr[i]는 i번째 전공책의 가격
 
 for i in range(N):
     a, b  = input().split()
     priceArr[i] = int(a)
-    alphaArr[i] = set(b)
+    alphaArr[i] = list(b)
     
 INF = 10 ** 15
 minPrice = INF
 for case in product([True, False], repeat = N):
     if isValid([alphaArr[i] for i in range(len(alphaArr)) if case[i]], T):
-        price = sum([priceArr[i]  for i in range(len(priceArr)) if case[i]])
+        price = sum([priceArr[i] for i in range(len(priceArr)) if case[i]])
         
         if minPrice > price:
             minPrice = price
